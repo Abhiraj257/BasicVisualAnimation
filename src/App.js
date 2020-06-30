7c29a7c7c14c93ac718d6b2f894bde2d0a338942
Abhishek <201751002@iiitvadodara.ac.in>
2020-06-30 18:34:23 +0530

    initial homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import gsap from 'gsap'
+
+import './styles/App.scss'
+import Header from './components/Header'
+import Banner from './components/Banner'
+import Cases from './components/Cases'
+import IntroOverlay from './components/introOverlay'
 
 function App() {
+  useEffect(() => {
+    // let vh = window.innerHeight * 0.01
+    // document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+    gsap.to('body', 0, { css: { visibility: 'visible' } })
+
+    const tl = gsap.timeline()
+
+    tl.from('.line span', 1.8, {
+      skewY: 7,
+      y: 70,
+      stagger: {
+        amount: 0.3
+      },
+      ease: 'power4.out'
+    })
+      .to('.overlay-top', 1.6, {
+        height: 0,
+        ease: 'expo.inOut',
+        stagger: {
+          amount: 0.4
+        }
+      })
+      .to('.overlay-bottom', 1.6, {
+        width: 0,
+        ease: 'expo.inOut',
+        stagger: {
+          amount: 0.4
+        },
+        delay: -0.8
+      })
+      .to('.intro-overlay', 0, { css: { display: 'none' } })
+      .from('.case-image img', 1.6, {
+        scale: 1.4,
+        stagger: {
+          amount: 0.4
+        },
+        delay: -2,
+        ease: 'expo.inOut'
+      })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <IntroOverlay />
+      <Header />
+      <Banner />
+      <Cases />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
